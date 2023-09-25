d93097d6742fb94f5afe97bc87b4aa75bee9a7ac
lc.chai <lc.chai.sg@gmail.com>
2023-09-25 21:41:18 +0800

    simple chat app completed

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require("./utils/users");
 
 const PORT = process.env.PORt || 3000;
 
@@ -12,28 +14,56 @@ const io = socketio(server);
 // set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+const botName = "Chatcord Bot";
+
 // run when a client connects
 io.on("connection", (socket) => {
     console.log("New websocket connection...");
 
-    // welcome current user
-    socket.emit("message", "Welcome to chatcord ...");
+    socket.on("joinRoom", ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
 
-    // broadcast when a user connects
-    // broadcast to every client except current client
-    socket.broadcast.emit("message", "A user has joined the chat");
+        socket.join(user.room);
 
-    // broadcast to everyone when client disconnects
-    socket.on("disconnect", () => {
-        io.emit("message", "A user has left the chat");
-    });
+        // welcome current user
+        socket.emit("message", formatMessage(botName, "Welcome to chatcord ..."));
+
+        // broadcast when a user connects
+        // broadcast to every client except current client
+        socket.broadcast.to(user.room).emit("message", formatMessage(botName, `${user.username} has joined the chat`));
+
+        // send users and room info
+        io.to(user.room).emit("roomusers", {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+    })
 
     // listen for chatMessage
     socket.on("chatMessage", (msg) => {
         console.log(msg);
-        io.emit("message", msg);
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit("message", formatMessage(user.username, msg));
+    });
+
+    // broadcast to everyone when client disconnects
+    socket.on("disconnect", () => {
+        const user = userLeave(socket.id);
+        console.log(user);
+
+        if (user) {
+            io.to(user.room).emit("message", formatMessage(botName, `${user.username} has left the chat`));
+
+            // send users and room info
+            io.to(user.room).emit("roomusers", {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
+        }
+
     });
 });
+
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,23 @@
 const chatForm = document.getElementById("chat-form");
 const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+
+// get username and room from URL
 
 const socket = io();
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
+
+// join chatroom
+socket.emit('joinRoom', { username, room });
+
+// get room and users
+socket.on("roomusers", ({ room, users }) => {
+    outputRoomName(room);
+    outputRoomUsers(users);
+})
 
 socket.on("message", (message) => {
     console.log(message);
@@ -32,9 +48,22 @@ chatForm.addEventListener("submit", (e) => {
 const displayMessage = (message) => {
     const div = document.createElement("div");
     div.classList.add("message");
-    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+    div.innerHTML = `<p class="meta">${message.username}<span style="padding-left:10px">${message.time}</span></p>
     <p class="text">
-        ${message}
+        ${message.text}
     </p>`;
     document.querySelector(".chat-messages").appendChild(div);
 };
+
+// add room name to DOM
+const outputRoomName = (room) => {
+    roomName.innerText = room;
+}
+
+// add users to DOM
+const outputRoomUsers = (users) => {
+    console.log(users);
+    userList.innerHTML = `
+        ${users.map(user => `<li>${user.username}</li>`).join("")}
+    `;
+}
